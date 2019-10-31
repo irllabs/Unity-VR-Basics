dd4fad97fa8ecc5e96d8bc87626f0d81c8f4c425
batchku <ali.momeni@shield.ai>
2019-10-31 15:13:05 -0400

    initial upload

---
+++ Unity Intro for Remix Reality/Assets/MyScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+    private int myNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Message with a GameObject name.
+        Debug.Log("Hello: " + gameObject.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the object forward along its z axis 1 unit/second.
+        Vector3 positionDelta = new Vector3(Random.Range(-.1f, .1f), Random.Range(-.1f, .1f), Random.Range(-.1f, .1f));
+        transform.Translate(positionDelta);
+    }
+}
